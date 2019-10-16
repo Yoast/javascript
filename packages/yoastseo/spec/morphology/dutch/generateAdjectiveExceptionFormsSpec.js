c32ba3573f65a52f58f2a79bef75e37cdeefcdd6
Fitria Marfuaty <fitriamarfuaty@Fitrias-MacBook-Pro.local>
2019-10-16 11:09:47 +0200

    fix formatting(2)

--- packages/yoastseo/spec/morphology/dutch/generateAdjectiveExceptionFormsSpec.js
+++ packages/yoastseo/spec/morphology/dutch/generateAdjectiveExceptionFormsSpec.js
@@ -4,29 +4,29 @@ import getMorphologyData from "../../specHelpers/getMorphologyData";
 const morphologyDataNL = getMorphologyData( "nl" ).nl;
 
 describe( "Test for checking adjective exceptions in Dutch", () => {
-    it( "creates forms for indeclinable exception words", () => {
-        expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, "acajou" ) ).toEqual(
-            "acajou"
-         );
-    } );
-    it( "creates forms for adjectives which only take partitive suffix -s", () => {
-        expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, "halal" ) ).toEqual(
-            [ "halals" ]
-        );
-    } );
-    it( "creates forms for adjectives ending in -en which take all adjective suffixes", () => {
-        expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, morphologyDataNL.addSuffixes.stemModifications, "feminien" ) ).toEqual(
-            [ "feminiener", "feminieners", "feminienere", "feminieneres", "feminienst", "feminienste", "feminiene", "feminiens" ]
-        );
-    } );
-    it( "creates forms for adjectives ending in -en which take 5 adjective suffixes (s, er, ers, st, ste)", () => {
-        expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, morphologyDataNL.addSuffixes.stemModifications, "even" ) ).toEqual(
-            [ "evens", "evener", "eveners", "evenst", "evenste" ]
-        )
-    } );
-    it("created forms for adjectives ending in -en which only take partitive and inflected suffixes", () => {
-        expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, morphologyDataNL.addSuffixes.stemModifications, "neopreen" ) ).toEqual(
-            [ "neopreens", "neoprene" ]
-        )
-    } )
+	it( "creates forms for indeclinable exception words", () => {
+		expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, "acajou" ) ).toEqual(
+			"acajou"
+		);
+	} );
+	it( "creates forms for adjectives which only take partitive suffix -s", () => {
+		expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, "halal" ) ).toEqual(
+			[ "halals" ]
+		);
+	} );
+	it( "creates forms for adjectives ending in -en which take all adjective suffixes", () => {
+		expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, morphologyDataNL.addSuffixes.stemModifications, "feminien" ) ).toEqual(
+			[ "feminiener", "feminieners", "feminienere", "feminieneres", "feminienst", "feminienste", "feminiene", "feminiens" ]
+		);
+	} );
+	it( "creates forms for adjectives ending in -en which take 5 adjective suffixes (s, er, ers, st, ste)", () => {
+		expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, morphologyDataNL.addSuffixes.stemModifications, "even" ) ).toEqual(
+			[ "evens", "evener", "eveners", "evenst", "evenste" ]
+		);
+	} );
+	it( "created forms for adjectives ending in -en which only take partitive and inflected suffixes", () => {
+		expect( generateAdjectiveExceptionForms( morphologyDataNL.adjectives, morphologyDataNL.addSuffixes.stemModifications, "neopreen" ) ).toEqual(
+			[ "neopreens", "neoprene" ]
+		);
+	} );
 } );
