81c9e85919122b864b9f96f80dd1bd2587dab081
iolandasequino <iolanda@yoast.com>
2020-02-11 10:48:44 +0100

    Add the list of Spanish words with their expected stems

--- packages/yoastseo/spec/morphology/spanish/stemSpec.js
+++ packages/yoastseo/spec/morphology/spanish/stemSpec.js
@@ -4,32 +4,58 @@ import getMorphologyData from "../../specHelpers/getMorphologyData";
 const morphologyDataES = getMorphologyData( "es" ).es;
 
 const wordsToStem = [
-	// Suffix step 1 (non-plurals in -s).
-	[ "", "" ],
-	// Suffix step 2 (clitic pronouns).
-	[ "", "" ],
-	// Suffix step 3 (diminutives).
-	[ "", "" ],
-	// Suffix step 4 verb suffixes preceded by y.
-	[ "", "" ],
-	// Suffix step 2 category a (-est)
-	[ "", "" ],
-	// Suffix step 2 category b (-st)
-	[ "", "" ],
-	// Suffix as defined in step 1 (-ern) that is not within the R1.
-	[ "", "" ],
-	// Suffix as defined in step 2 (-est) that is not within the R1.
-	[ "", "" ],
-	// A word without an R1.
-	[ "", "" ],
-	// An irregular verb.
-	[ "", "" ],
-	// A word with a vowel that should be treated like a consonant
-	[ "", "" ],
+	// // Input a word that ends in -s but is not a plural.
+	// [ "caos", "caos" ],
+	// [ "gas", "gas" ],
+	// // Input a word that ends with a clitic pronoun and is on the list of words that end like pronouns suffixes but are not verbs.
+	// [ "anime", "anim" ],
+	// [ "abuela", "abuel" ],
+	// // Input a word that ends with a clitic pronoun and is a verb.
+	// [ "abofarse", "abof" ],
+	// [ "mirame", "mir" ],
+	// // Input a word that does not ends with a clitic pronoun and is on the exceptions full forms list.
+	// [ "sacratísimo", "sagrad" ],
+	// [ "veamos", "ver" ],
+	// // Input a word that looks like a diminutive but is not.
+	// [ "acólito", "acolit" ],
+	// [ "amalecitas", "amalecit" ],
+	// // Input a word that is on the diminutive exceptions list.
+	// [ "reicito", "rey" ],
+	// [ "lucecita", "luz" ],
+	// // Input a word that is a canonical diminutive.
+	// [ "puertecita", "puert" ],
+	// [ "ventita", "vent" ],
+	// // Input a word that ends in a suffix preceded by uy.
+	// [ "excluyendo", "exclu" ],
+	// [ "atribuyes", "atribu" ],
+	// // Input a word that undergoes stem modification changes.
+	// [ "recuerdan", "record" ],
+	// [ "comienzo", "comenz" ],
+	// // Input a word that ends in a common verb suffix.
+	// [ "saltaron", "salt" ],
+	// [ "revocares", "revoc" ],
+	// // Input a word that ends in -os, -s, -a, -o, -á, -í,-ó, -é, -e.
+	// [ "agostinas", "agostin" ],
+	// [ "boboré", "bobor" ],
+	// // Input a word that is on the stems that belong together list.
+	// [ "dollar", "dolar" ],
+	// [ "chalets", "chale" ],
+	// // Input a word that ends in -en, -es, -éis, -emos and is not preceded by gu.
+	// [ "valéis", "val" ],
+	// [ "dirigen", "dirig" ],
+	// // Input a word that ends in -en, -es, -éis, -emos and is preceded by gu.
+	// [ "distinguen", "distingu" ],
+	// [ "alarguemos", "alarg" ],
+	// // Input a word that looks like a verb form byt it's not.
+	// [ "cabalgada", "cabalgad" ],
+	// [ "abacerías", "abaceri" ],
+	// // Input a word that looks like a verb form and is on the list of stems that belong together.
+	// [ "san", "san" ],
+	// [ "virgen", "virgen" ],
 ];
 
 describe( "Test for stemming Spanish words", () => {
-	it( "stems Spanish nouns", () => {
+	it( "stems Spanish words", () => {
 		wordsToStem.forEach( wordToStem => expect( stem( morphologyDataES, wordToStem[ 0 ] ) ).toBe( wordToStem[ 1 ] ) );
 	} );
 } );
