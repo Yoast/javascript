71ce912f845f7142f7feda814aba618d8747540e
Alexander Botteram <a.botteram@gmail.com>
2020-02-25 10:39:56 +0100

    Added --watch command

--- packages/components/webpack.config.js
+++ packages/components/webpack.config.js
@@ -1,25 +1,27 @@
 const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
 const glob = require( "glob" );
 
-module.exports = {
-	mode: "production",
-	entry: glob.sync( "./src/**/*.css" ),
-	plugins: [
-		new MiniCssExtractPlugin( {
-			filename: "styles.css",
-		} ),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: MiniCssExtractPlugin.loader,
-					},
-					"css-loader",
-				],
-			},
+module.exports = () => {
+	return {
+		mode: "production",
+		entry: glob.sync( "./src/**/*.css" ),
+		plugins: [
+			new MiniCssExtractPlugin( {
+				filename: "styles.css",
+			} ),
 		],
-	},
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+						},
+						"css-loader",
+					],
+				},
+			],
+		},
+	}
 };
