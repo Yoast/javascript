7160ee4325416a3ec371a726176730982ae64854
Manuel <manuel@yoast.com>
2020-10-06 20:36:02 +0200

    Adapt assessment specs

--- packages/yoastseo/spec/assessments/SingularPluralAssessmentPrototypeSpec.js
+++ packages/yoastseo/spec/assessments/SingularPluralAssessmentPrototypeSpec.js
@@ -22,19 +22,25 @@ describe( "Tests for the ranking intention assessment for English", function() {
 			{ keyword: "plant pots" } );
 		const researcher = new Researcher( paper );
 		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
-		expect( result.getScore() ).toBe( 6 );
-		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: " +
-			"Your text does not reflect any particular ranking intention. " +
-			"If your keywords is singular, use more singular occurrences; if your keyphrase is plural, use more plural occurrences!" );
+		expect( result.getScore() ).toBe( 9 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>:" +
+			" Your text reflects your ranking intention. Good job!" );
 	} );
 	it( "runs the ranking intention on the paper with keyword", function() {
 		const paper = new Paper( "There is more than a pot you can choose for your plant. The pot with tribal pattern is our bestseller.",
 			{ keyword: "plant pots" } );
 		const researcher = new Researcher( paper );
 		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
-		expect( result.getScore() ).toBe( 3 );
-		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: " +
-			"Your text does not reflect your ranking intention. Change your keyphrase occurrences!" );
+		expect( result.getScore() ).toBe( 6 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: Your text does not reflect any particular ranking intention. If your keywords is singular, use more singular occurrences; if your keyphrase is plural, use more plural occurrences!" );
+	} );
+	it( "runs the ranking intention on the paper with keyword", function() {
+		const paper = new Paper( "A paper with no keyphrase occurrence.",
+			{ keyword: "plant pots" } );
+		const researcher = new Researcher( paper );
+		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
+		expect( result.getScore() ).toBe( 0 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: Include your keyphrase in the text so that we can check ranking intention." );
 	} );
 } );
 
@@ -45,15 +51,40 @@ describe( "A test for marking the keyword", function() {
 		const researcher = new Researcher( paper );
 		SingularPluralAssessment.getResult( paper, researcher, i18n );
 		const expected = [
-			{ _properties: { marked: "An ethnic model of <yoastmark class='yoast-text-mark'>plant pots</yoastmark>, " +
-						"ethnic model of <yoastmark class='yoast-text-mark'>plants pots</yoastmark>.",
-			original: "An ethnic model of plant pots, ethnic model of plants pots." } },
-			{ _properties: { marked: "An ethnic model of <yoastmark class='yoast-text-mark'>plant pots</yoastmark>, " +
-						"ethnic model of <yoastmark class='yoast-text-mark'>plants pots</yoastmark>.",
-			original: "An ethnic model of plant pots, ethnic model of plants pots." } },
-			{ _properties: { marked: "An ethnic model of <yoastmark class='yoast-text-mark'>plant pots</yoastmark>, " +
-						"ethnic model of <yoastmark class='yoast-text-mark'>plants pots</yoastmark>.",
-			original: "An ethnic model of plant pots, ethnic model of plants pots." } } ];
-		expect( SingularPluralAssessment.getMarks() ).toEqual( expected );
+			{
+				_properties: {
+					marked: "An ethnic model of plant pots, ethnic model of <yoastmark class='yoast-text-mark'>plants</yoastmark> pots.",
+					original: "An ethnic model of plant pots, ethnic model of plants pots.",
+				},
+			} ];
+		expect( SingularPluralAssessment.getMarks( paper ) ).toEqual( expected );
+	} );
+} );
+
+describe( "Checks if the assessment is applicable", function() {
+	const SingularPluralAssessment = new SingularPluralAssessmentPrototype();
+
+	it( "is applicable when there is a text of 100+ words and the keyphrase is set", function() {
+		const mockPaper = new Paper( "word ".repeat( 100 ) + "fluffy cat", { keyword: "fluffy cat" } );
+		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
+		expect( isApplicable ).toBe( true );
+	} );
+
+	it( "is not applicable when there is no text", function() {
+		const mockPaper = new Paper( "", { keyword: "fluffy cat" } );
+		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
+		expect( isApplicable ).toBe( false );
+	} );
+
+	it( "is not applicable when the text is shorter than 100 words", function() {
+		const mockPaper = new Paper( "word ".repeat( 50 ) + "fluffy cat", { keyword: "fluffy cat" } );
+		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
+		expect( isApplicable ).toBe( false );
+	} );
+
+	it( "is not applicable when no keyphrase is set", function() {
+		const mockPaper = new Paper( "word ".repeat( 100 ) );
+		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
+		expect( isApplicable ).toBe( false );
 	} );
 } );
