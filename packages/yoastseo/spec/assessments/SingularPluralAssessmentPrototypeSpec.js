289fe471725aa9f4d3b1840111eb2054d4612bf7
FAMarfuaty <aida@yoast.com>
2020-10-07 16:53:57 +0200

    Adjust the specs to accommodate the changes in the files

--- packages/yoastseo/spec/assessments/SingularPluralAssessmentPrototypeSpec.js
+++ packages/yoastseo/spec/assessments/SingularPluralAssessmentPrototypeSpec.js
@@ -3,15 +3,18 @@ import SingularPluralAssessmentPrototype from "../../src/assessments/seo/Singula
 import Researcher from "../../src/researcher";
 import Paper from "../../src/values/Paper.js";
 import factory from "../specHelpers/factory.js";
+import getMorphologyData from "../specHelpers/getMorphologyData";
 
+const morphologyDataEN = getMorphologyData( "en" );
 
 const i18n = factory.buildJed();
 
 describe( "Tests for the ranking intention assessment for English", function() {
 	it( "runs the ranking intention on the paper with keyword", function() {
 		const paper = new Paper( "There are many pots for plant that you can choose. Pots with tribal pattern is our bestseller.",
-			{ keyword: "plant pots" } );
+			{ keyword: "plant pots", locale: "en" } );
 		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyDataEN );
 		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
 		expect( result.getScore() ).toBe( 9 );
 		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>:" +
@@ -19,8 +22,9 @@ describe( "Tests for the ranking intention assessment for English", function() {
 	} );
 	it( "runs the ranking intention on the paper with keyword", function() {
 		const paper = new Paper( "There are many pots for plant that you can choose. The pot with tribal pattern is our bestseller.",
-			{ keyword: "plant pots" } );
+			{ keyword: "plant pots", locale: "en" } );
 		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyDataEN );
 		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
 		expect( result.getScore() ).toBe( 9 );
 		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>:" +
@@ -28,19 +32,34 @@ describe( "Tests for the ranking intention assessment for English", function() {
 	} );
 	it( "runs the ranking intention on the paper with keyword", function() {
 		const paper = new Paper( "There is more than a pot you can choose for your plant. The pot with tribal pattern is our bestseller.",
-			{ keyword: "plant pots" } );
+			{ keyword: "plant pots", locale: "en" } );
 		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyDataEN );
 		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
 		expect( result.getScore() ).toBe( 6 );
-		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: Your text does not reflect any particular ranking intention. If your keywords is singular, use more singular occurrences; if your keyphrase is plural, use more plural occurrences!" );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: Your text does not reflect " +
+			"any particular ranking intention. If your keywords is singular, use more singular occurrences; " +
+			"if your keyphrase is plural, use more plural occurrences!" );
 	} );
 	it( "runs the ranking intention on the paper with keyword", function() {
-		const paper = new Paper( "A paper with no keyphrase occurrence.",
-			{ keyword: "plant pots" } );
+		const paper = new Paper( "Tortie or tortoiseshell  cats are very special cats. 99% of torties are female.",
+			{ keyword: "tortie cat", locale: "en" } );
 		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyDataEN );
+		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
+		expect( result.getScore() ).toBe( 3 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: " +
+			"Your text does not reflect your ranking intention. Change your keyphrase occurrences!" );
+	} );
+	it( "runs the ranking intention on the paper without keyword occurrence", function() {
+		const paper = new Paper( "A paper with no keyphrase occurrence.",
+			{ keyword: "plant pots", locale: "en" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyDataEN );
 		const result = new SingularPluralAssessmentPrototype().getResult( paper, researcher, i18n );
 		expect( result.getScore() ).toBe( 0 );
-		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: Include your keyphrase in the text so that we can check ranking intention." );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/33v' target='_blank'>Ranking intention</a>: " +
+			"Include your keyphrase in the text so that we can check ranking intention." );
 	} );
 } );
 
@@ -49,6 +68,7 @@ describe( "A test for marking the keyword", function() {
 		const SingularPluralAssessment = new SingularPluralAssessmentPrototype();
 		const paper = new Paper( "An ethnic model of plant pots, ethnic model of plants pots.", { keyword: "plant pots" }  );
 		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyDataEN );
 		SingularPluralAssessment.getResult( paper, researcher, i18n );
 		const expected = [
 			{
@@ -65,25 +85,30 @@ describe( "Checks if the assessment is applicable", function() {
 	const SingularPluralAssessment = new SingularPluralAssessmentPrototype();
 
 	it( "is applicable when there is a text of 100+ words and the keyphrase is set", function() {
-		const mockPaper = new Paper( "word ".repeat( 100 ) + "fluffy cat", { keyword: "fluffy cat" } );
+		const mockPaper = new Paper( "word ".repeat( 100 ) + "fluffy cat", { keyword: "fluffy cat", locale: "en" } );
 		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
 		expect( isApplicable ).toBe( true );
 	} );
 
 	it( "is not applicable when there is no text", function() {
-		const mockPaper = new Paper( "", { keyword: "fluffy cat" } );
+		const mockPaper = new Paper( "", { keyword: "fluffy cat", locale: "en" } );
 		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
 		expect( isApplicable ).toBe( false );
 	} );
 
 	it( "is not applicable when the text is shorter than 100 words", function() {
-		const mockPaper = new Paper( "word ".repeat( 50 ) + "fluffy cat", { keyword: "fluffy cat" } );
+		const mockPaper = new Paper( "word ".repeat( 50 ) + "fluffy cat", { keyword: "fluffy cat", locale: "en" } );
 		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
 		expect( isApplicable ).toBe( false );
 	} );
 
 	it( "is not applicable when no keyphrase is set", function() {
 		const mockPaper = new Paper( "word ".repeat( 100 ) );
+		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
+		expect( isApplicable ).toBe( false );
+	} );
+	it( "is not applicable when locale is not English", function() {
+		const mockPaper = new Paper( "kucing jantan ".repeat( 100 ), { keyword: "kucing", locale: "id" } );
 		const isApplicable = SingularPluralAssessment.isApplicable( mockPaper );
 		expect( isApplicable ).toBe( false );
 	} );
