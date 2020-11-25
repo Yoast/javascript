6fc8944e4635c67b7a3df373e376c6454cd782db
iolandasequino <iolanda@yoast.com>
2020-11-25 12:05:25 +0100

    edit specs and remove console.logs

--- packages/yoastseo/spec/researches/hungarian/passiveVoice/SentencePartSpec.js
+++ packages/yoastseo/spec/researches/hungarian/passiveVoice/SentencePartSpec.js
@@ -1,18 +1,10 @@
 import HungarianSentencePart from "../../../../src/researches/hungarian/passiveVoice/SentencePart.js";
 
-describe( "creates a hungarian sentence part", function() {
-	it( "makes sure the hungarian sentence part inherits all functions", function() {
-		const mockPart = new HungarianSentencePart( "Hungarian text." );
-		expect( mockPart.getSentencePartText() ).toBe( "Hungarian text." );
-	} );
-
-	// describe( "gets morphological participles of hungarian sentence", function() {
-	// 	it( "returns morphological participles", function() {
-	// 		const mockPart = new HungarianSentencePart( "Minden játékosnak 3 játék garantálódik", "hu" );
-	// 		const foundParticiples = mockPart.getParticiples()[ 0 ];
-	// 		expect( foundParticiples.getParticiple() ).toEqual( "garantálódik" );
-	// 	} );
-	// } );
+	describe( "creates a hungarian sentence part", function() {
+		it( "makes sure the hungarian sentence part inherits all functions", function() {
+			const mockPart = new HungarianSentencePart( "Hungarian text." );
+			expect( mockPart.getSentencePartText() ).toBe( "Hungarian text." );
+		} );
 
 	describe( "gets periprhastic participles of hungarian sentence", function() {
 		it( "returns periprhastic participles ending in -va", function() {
