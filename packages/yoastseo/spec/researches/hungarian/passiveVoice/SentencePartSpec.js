82421b8afe7bb8bb08a30b770d7a4eae89dce66e
Agnieszka Szuba <agnieszka@yoast.com>
2020-11-25 13:38:54 +0100

    Fix grunt check errors

--- packages/yoastseo/spec/researches/hungarian/passiveVoice/SentencePartSpec.js
+++ packages/yoastseo/spec/researches/hungarian/passiveVoice/SentencePartSpec.js
@@ -1,10 +1,10 @@
 import HungarianSentencePart from "../../../../src/researches/hungarian/passiveVoice/SentencePart.js";
 
-	describe( "creates a hungarian sentence part", function() {
-		it( "makes sure the hungarian sentence part inherits all functions", function() {
-			const mockPart = new HungarianSentencePart( "Hungarian text." );
-			expect( mockPart.getSentencePartText() ).toBe( "Hungarian text." );
-		} );
+describe( "creates a hungarian sentence part", function() {
+	it( "makes sure the hungarian sentence part inherits all functions", function() {
+		const mockPart = new HungarianSentencePart( "Hungarian text." );
+		expect( mockPart.getSentencePartText() ).toBe( "Hungarian text." );
+	} );
 
 	describe( "gets periprhastic participles of hungarian sentence", function() {
 		it( "returns periprhastic participles ending in -va", function() {
@@ -28,7 +28,7 @@ import HungarianSentencePart from "../../../../src/researches/hungarian/passiveV
 			const foundParticiples = mockPart.getParticiples()[ 0 ];
 			expect( foundParticiples.getParticiple() ).toEqual( "megfinanszírozásra" );
 		} );
-    } );
+	} );
 
 	describe( "gets periprhastic participles of hungarian sentence", function() {
 		it( "returns periprhastic participles ending in -re", function() {
@@ -38,6 +38,5 @@ import HungarianSentencePart from "../../../../src/researches/hungarian/passiveV
 		} );
 	} );
 } );
-
 
 
