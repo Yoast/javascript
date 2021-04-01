005f7fd4add67b44d3f6eec5c1052ddf4087e775
Josee Wouters <info@joseewouters.nl>
2021-04-01 15:22:53 +0200

    Add Settings app

---
+++ apps/settings-app/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	purge: [ "./src/**/*.{js,jsx,ts,tsx}" ],
+	darkMode: false, // Or 'media' or 'class'.
+	theme: {
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
